3b38be6424fbfee9905ee7789b6e46c4c623344d
Reymon99 <s.maje@udla.edu.co>
2020-05-07 16:54:18 -0500

    [ADDED] Main of Application

---
+++ src/gui/Main.java
@@ -0,0 +1,17 @@
+package gui;
+
+import javax.swing.*;
+
+public class Main extends JFrame {
+    private Main() {
+        super("Sorting Comparator");
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setContentPane(new JPanel());
+        pack();
+        setLocationRelativeTo(null);
+    }
+
+    public static void main(String[] args) {
+        new Main().setVisible(true);
+    }
+}
