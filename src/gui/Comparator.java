c74c6a4cdbbc47fa025453f9f64d227d313281b1
Reymon99 <s.maje@udla.edu.co>
2020-05-07 20:42:28 -0500

    [MODIFIED] comparison panel

--- src/gui/Comparator.java
+++ src/gui/Comparator.java
@@ -6,6 +6,8 @@ import org.constrains.Weight;
 
 import javax.swing.*;
 import java.awt.*;
+import java.util.Objects;
+import java.util.Optional;
 
 public class Comparator extends JPanel {
 
@@ -15,12 +17,50 @@ public class Comparator extends JPanel {
     }
 
     private void init() {
+        TableComparator table = new TableComparator();
         Constrains.addComp(
-                new View(new TableComparator(), this),
+                new View(table, this),
                 new Rectangle(0, 0, 1, 1),
                 new Weight(1, 1),
                 new Insets(10, 10, 10, 10),
                 new Point(GridBagConstraints.CENTER, GridBagConstraints.BOTH)
         );
+        Constrains.addComp(
+                new View(comparator(), this),
+                new Rectangle(0, 1, 1, 1),
+                new Weight(1, 1),
+                new Insets(30, 10, 10, 10),
+                new Point(GridBagConstraints.CENTER, GridBagConstraints.NONE)
+        );
+    }
+
+    private JPanel comparator() {
+        JPanel panel = new JPanel(new FlowLayout(FlowLayout.CENTER, 5, 5));
+        panel.setBorder(BorderFactory.createTitledBorder("Comparator"));
+        panel.setOpaque(false);
+        JComboBox<Methods> comparatorOne = new JComboBox<>(new DefaultComboBoxModel<>(Methods.values()));
+        JComboBox<Methods> comparatorTwo = new JComboBox<>(new DefaultComboBoxModel<>(Methods.values()));
+        comparatorOne.setSelectedIndex(-1);
+        comparatorTwo.setSelectedIndex(-1);
+        comparatorOne.addActionListener(e -> {
+            if (Objects.equals(
+                    comparatorOne.getSelectedItem(),
+                    Optional.ofNullable(comparatorTwo.getSelectedItem()).orElse("")
+            ))
+                comparatorOne.setSelectedIndex(-1);
+        });
+        comparatorTwo.addActionListener(e -> {
+            if (Objects.equals(
+                    comparatorTwo.getSelectedItem(),
+                    Optional.ofNullable(comparatorOne.getSelectedItem()).orElse("")
+            ))
+                comparatorTwo.setSelectedIndex(-1);
+        });
+        JButton button = new JButton("Comparator");
+        button.addActionListener(e -> {});
+        panel.add(comparatorOne);
+        panel.add(comparatorTwo);
+        panel.add(button);
+        return panel;
     }
 }
