4f9a59060177f14052722722157740f5dd659fed
Sergio Majé <s.maje@udla.edu.co>
2020-07-19 03:44:07 -0500

    [ADDED] number of pointers

--- src/gui/Methods.java
+++ src/gui/Methods.java
@@ -1,19 +1,25 @@
 package gui;
 
 public enum Methods {
-    BUBBLE("Simple Bubble"),
-    IMPROVED_BUBBLE("Improved Bubble"),
-    OPTIMIZED_BUBBLE("Optimized Bubble"),
-    QUICKSORT("QuickSort"),
-    SHELLSORT("ShellSort"),
-    RADIXSORT("RadixSort"),
-    SELECTION("Selection"),
-    INSERTION("Insertion"),
-    MERGESORT("MergeSort");
+    BUBBLE("Simple Bubble", 1),
+    IMPROVED_BUBBLE("Improved Bubble", 1),
+    OPTIMIZED_BUBBLE("Optimized Bubble", 1),
+    QUICKSORT("QuickSort", 1),
+    SHELLSORT("ShellSort", 2),
+    RADIXSORT("RadixSort", 1),
+    SELECTION("Selection", 1),
+    INSERTION("Insertion", 1),
+    MERGESORT("MergeSort", 1);
     private final String name;
+    private final int pointers;
 
-    Methods(String name) {
+    Methods(String name, int pointers) {
         this.name = name;
+        this.pointers = pointers;
+    }
+
+    public int getPointers() {
+        return pointers;
     }
 
     @Override
